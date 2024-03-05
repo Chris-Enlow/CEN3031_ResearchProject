772867fa256eb962329fdaa46dc2eeb180f8fafa
Chloe Fandino <chloefandino@ufl.edu>
2024-03-05 16:05:11 -0500

    change title on first page

--- DineQuest/src/components/Auth.vue
+++ DineQuest/src/components/Auth.vue
@@ -26,7 +26,7 @@ const handleLogin = async () => {
 <template>
   <form class="row flex-center flex" @submit.prevent="handleLogin">
     <div class="col-6 form-widget">
-      <h1 class="header">Supabase + Vue 3</h1>
+      <h1 class="header">DineQuest!</h1>
       <p class="description">Sign in via magic link with your email below</p>
       <div>
         <input class="inputField" required type="email" placeholder="Your email" v-model="email" />
