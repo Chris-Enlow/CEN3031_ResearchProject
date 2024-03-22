63878dbc664350c6b01825e8a59ca4b78d3d5576
Chloe Fandino <chloefandino@ufl.edu>
2024-03-22 15:45:13 -0400

    added account message

--- DineQuest/src/components/Account.vue
+++ DineQuest/src/components/Account.vue
@@ -59,6 +59,7 @@ async function signOut() {
 <template>
   <form class="form-widget" @submit.prevent="updateProfile">
     <div>
+      <h1 class="header">Welcome to your DineQuest account!</h1>
       <label for="email">Email</label>
       <input id="email" type="text" :value="session.user.email" disabled />
     </div>
